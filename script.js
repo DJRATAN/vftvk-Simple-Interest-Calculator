6dfe92011f74c64581ec68fc8ce0ad6dc6bab871
Ratan Prajapati <ratanprajapati1242@gmail.com>
2022-01-19 14:57:48 +0530

    updating index.html

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+      alert("Enter a Positive Number");
+    }
+    var rate = document.getElementById("rate").value;
+    updateRate();
+    var years = document.getElementById("years").value;
+  
+    var interest = (principal * years * rate) / 100;
+  
+    var year = new Date().getFullYear() + parseInt(years);
+  
+    var amount = parseInt(principal) + parseInt(interest);
+    document.getElementById("result").innerHTML =
+      "<br><br>If you deposit " +
+      principal +
+      ",<br>at an interest rate of " +
+      rate +
+      "%<br>You will receive an amount of " +
+      amount +
+      ",<br>in the year " +
+      year +
+      "<br>";
+  }
+  
+  function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+  }
+  
